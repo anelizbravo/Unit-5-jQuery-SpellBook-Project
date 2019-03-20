7513cc77fe6d9e16225d3aaa4799c24fc07eb770
anelizbravo <43544121+anelizbravo@users.noreply.github.com>
2019-03-20 10:06:46 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#hide").click(function(){
+ $("#sleep-image").toggle();
+});
+
+
+
+//Spell #2
+
+$("#slide").click(function(){
+ $("#wish-image").slideToggle();
+});
+
+
+
+//Spell #3
+
+$("#fade").click(function(){
+ $("#tea-image").fadeToggle();
+});
+
+
+
